cca42b72620d010f42c580eccdfb577385609c0f
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-06-29 03:59:10 +0300

    Add files via upload

---
+++ 03. Elevator/Program.cs
@@ -0,0 +1,19 @@
+﻿int numberOfPeople = int.Parse(Console.ReadLine());
+int elevatorCapacity = int.Parse(Console.ReadLine());
+int courses;
+
+if (numberOfPeople <= elevatorCapacity)
+{
+    courses = 1;
+}
+else
+{
+    courses = numberOfPeople / elevatorCapacity;
+
+    if (numberOfPeople % elevatorCapacity != 0)
+    {
+        courses += 1;
+    }
+}
+
+Console.WriteLine(courses);
