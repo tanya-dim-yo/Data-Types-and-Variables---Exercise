aeba91ebd31f722df815fba89ce324fdb67b7718
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-06-29 03:59:50 +0300

    Add files via upload

---
+++ 04. Sum of Chars/Program.cs
@@ -0,0 +1,10 @@
+﻿int numberOfLetters = int.Parse(Console.ReadLine());
+int sum = 0;
+
+for (int currentLetter = 1; currentLetter <= numberOfLetters; currentLetter++)
+{
+    char letter = char.Parse(Console.ReadLine());
+    sum += (int)letter;
+}
+
+Console.WriteLine($"The sum equals: {sum}");
