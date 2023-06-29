e4b7466c36d0a3d02a3aecfe237ced8373826880
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-06-29 04:01:14 +0300

    Add files via upload

---
+++ 05. Print Part Of ASCII Table/Program.cs
@@ -0,0 +1,7 @@
+﻿int startNumber = int.Parse(Console.ReadLine());
+int endNumber = int.Parse(Console.ReadLine());
+
+for (int i = startNumber; i <= endNumber; i++)
+{
+    Console.Write($"{(char)i} ");
+}
