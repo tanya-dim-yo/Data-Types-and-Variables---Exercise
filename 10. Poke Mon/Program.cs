b4e2944b9b989b0d649b48b9247486ec3c9f8d2e
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-07-02 05:36:18 +0300

    Add files via upload

---
+++ 10. Poke Mon/Program.cs
@@ -0,0 +1,19 @@
+﻿int pokemonPower = int.Parse(Console.ReadLine());
+int targetDistance = int.Parse(Console.ReadLine());
+int exhaustionFactor = int.Parse(Console.ReadLine());
+double percentage = pokemonPower * 0.5;
+int targetsPoked = 0;
+
+while (pokemonPower >= targetDistance)
+{
+    pokemonPower -= targetDistance;
+    targetsPoked++;
+
+    if (pokemonPower == percentage && exhaustionFactor != 0)
+    {
+        pokemonPower /= exhaustionFactor;
+    }
+}
+
+Console.WriteLine(pokemonPower);
+Console.WriteLine(targetsPoked);
