35fca6bb96079524912fafb1c476f26adc5ead1a
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-07-02 05:33:37 +0300

    Add files via upload

---
+++ 08. Beer Kegs/Program.cs
@@ -0,0 +1,20 @@
+﻿int numberOfKegs = int.Parse(Console.ReadLine());
+string bestModel = "";
+double maxVolume = 0;
+
+for (int i = 1; i <= numberOfKegs; i++)
+{
+    string model = Console.ReadLine();
+    float radius = float.Parse(Console.ReadLine());
+    int height = int.Parse(Console.ReadLine());
+
+    double volume = Math.PI * Math.Pow(radius, 2) * height;
+
+    if (volume > maxVolume)
+    {
+        maxVolume = volume;
+        bestModel = model;
+    }
+}
+
+Console.WriteLine(bestModel);
