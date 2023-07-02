39d09061e4d8ddb84f5fb49d6929f8a8316cdbef
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-07-02 05:34:45 +0300

    Add files via upload

---
+++ 09. Spice Must Flow/Program.cs
@@ -0,0 +1,17 @@
+﻿int startingYield = int.Parse(Console.ReadLine());
+int extractedSpices = 0;
+int days = 0;
+
+while (startingYield >= 100)
+{
+    days++;
+    extractedSpices += startingYield - 26;
+    startingYield -= 10;
+}
+
+if (days > 0)
+{
+    extractedSpices -= 26;
+}
+Console.WriteLine(days);
+Console.WriteLine(extractedSpices);
