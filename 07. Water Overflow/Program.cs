977b2716ab656c9949508aa15a9b1bc77dd7f29e
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-07-02 05:32:55 +0300

    Add files via upload

---
+++ 07. Water Overflow/Program.cs
@@ -0,0 +1,19 @@
+﻿int n = int.Parse(Console.ReadLine());
+int capacity = 255;
+int filledWater = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    int litters = int.Parse(Console.ReadLine());
+
+    if (litters > (capacity - filledWater))
+    {
+        Console.WriteLine("Insufficient capacity!");
+    }
+    else
+    {
+        filledWater += litters;
+    }
+}
+
+Console.WriteLine(filledWater);
