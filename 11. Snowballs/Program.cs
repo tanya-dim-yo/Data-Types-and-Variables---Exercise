637cdcfa35818ae5c7eb4a7daa842d28c9cf23ee
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-07-03 05:14:37 +0300

    Add files via upload

---
+++ 11. Snowballs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+
+int snowballsNumber = int.Parse(Console.ReadLine());
+int bestSnow = 0;
+int bestTime = 0;
+int bestQuality = 0;
+BigInteger bestValue = 0;
+
+for (int i = 1; i <= snowballsNumber; i++)
+{
+    int snowballSnow = int.Parse(Console.ReadLine());
+    int snowballTime = int.Parse(Console.ReadLine());
+    int snowballQuality = int.Parse(Console.ReadLine());
+
+    BigInteger snowballValue = BigInteger.Pow(snowballSnow / snowballTime, snowballQuality);
+
+    if (snowballValue > bestValue)
+    {
+        bestValue = snowballValue;
+        bestQuality = snowballQuality;
+        bestTime = snowballTime;
+        bestSnow = snowballSnow;
+    }
+}
+
+Console.WriteLine($"{bestSnow} : {bestTime} = {bestValue} ({bestQuality})");
